9f0b61d6fcf8ae5871a1b9137419e0d2ed8270f5
Allan Lewis <allan.e.lewis@gmail.com>
2026-03-08 10:43:37 -0400

    #232: converge linux

--- scripts/proxmox-runway.sh
+++ scripts/proxmox-runway.sh
@@ -6,4 +6,4 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT_DIR"
 
 mkdir -p artifacts/l0
-ansible-playbook ansible/l0/runway.yaml
+ansible-playbook ansible/perform-runway-checks.yaml
