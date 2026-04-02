d8c662af01e3fdb88154f8e0f49a44a5186f54cd
Allan Lewis <allan.e.lewis@gmail.com>
2026-04-02 14:12:15 -0400

    #227: nixy containers

--- nixos/profiles/containers/it-tools/default.nix
+++ nixos/profiles/containers/it-tools/default.nix
@@ -1,6 +1,6 @@
 {
   services.homelab.containers.it-tools = {
-    image = "corentinth/it-tools@sha256:6f177c156b9466610e0f2093e24668b78da501c66f0054f98bccb582b74ab26b";
+    image = "corentinth/it-tools:2024.10.22-7ca5933@sha256:6f177c156b9466610e0f2093e24668b78da501c66f0054f98bccb582b74ab26b";
     port = 8386;
     internalPort = 80;
   };
--- nixos/profiles/containers/nginx/default.nix
+++ nixos/profiles/containers/nginx/default.nix
@@ -1,6 +1,6 @@
 {
   services.homelab.containers.nginx = {
-    image = "nginx@sha256:42e026ae5315aa0deec22fb00c364fc5ec8d9af1c4833ad5317e2a433e4de0df";
+    image = "nginx:1.28.2@sha256:42e026ae5315aa0deec22fb00c364fc5ec8d9af1c4833ad5317e2a433e4de0df";
     port = 80;
   };
 }
