94cc36d5498a50c455a04e56f81fd3e953b8c6b0
Allan Lewis <allan.e.lewis@gmail.com>
2026-04-02 11:45:56 -0400

    #240: re-structure

--- terraform/nixos/apps/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "gitops-homelab-orchestrator-tf"
-    key    = "l2/nixos_apps/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- terraform/nixos/media/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "gitops-homelab-orchestrator-tf"
-    key    = "l2/nixos_media/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
