a213a0bd013f82155adced02b040ca68ad072a1f
Allan Lewis <allan.e.lewis@gmail.com>
2026-04-02 13:49:15 -0400

    #227: nixy containers

--- nixos/profiles/observability/default.nix
+++ nixos/profiles/observability/default.nix
@@ -18,7 +18,7 @@
 
   services.homelab.containers.homelab-metrics = {
     port = 9102;
-    image = "docker.io/allanelewis/homelab-metrics@sha256:0ec3727dece3b49e0cb2cddc39650670c87b3bf4bb9c1024244ac5329ad5719f";
+    image = "docker.io/allanelewis/homelab-metrics:v2026.04.0@sha256:0ec3727dece3b49e0cb2cddc39650670c87b3bf4bb9c1024244ac5329ad5719f";
   };
 
 }
