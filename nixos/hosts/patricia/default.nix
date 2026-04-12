6c4b72ab61e1d0bd652cfe7de3edc5060cf2620d
Allan Lewis <allan.e.lewis@gmail.com>
2026-04-12 13:21:28 -0400

    r stack

--- nixos/hosts/patricia/default.nix
+++ nixos/hosts/patricia/default.nix
@@ -13,7 +13,31 @@ in
 
   networking.hostName = hostName;
 
-  services.homelab.managedState.enable = lib.mkForce false;
+  homelab.managedDirectories.entries = {
+    prowlarrConfig = {
+      local = "/etc/prowlarr";
+      remote = "${defaultRemoteNasPerHostBackupVolume}/prowlarr/config";
+      restore = true;
+      backup = true;
+      owner = "lab";
+      group = "lab";
+      mode = "0755";
+    };
+  };
+
+  fileSystems = {
+    "/data/media-library" = {
+      device = "192.168.86.220:/mnt/pool1/media-acquisition";
+      fsType = "nfs";
+      options = [
+        "rw"
+        "nofail"
+        "_netdev"
+        "x-systemd.requires=network-online.target"
+        "x-systemd.after=network-online.target"
+      ];
+    };
+  };
 
   system.stateVersion = "25.11";
 }
