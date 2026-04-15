98e7df6f1cf5a958a198765fd1f529abff37c360
allan-lewis <allan.e.lewis@gmail.com>
2026-04-14 20:44:08 -0400

    jellyfin

--- nixos/modules/oci-containers/jellyfin.nix
+++ nixos/modules/oci-containers/jellyfin.nix
@@ -1,4 +1,4 @@
-{ nasRootFolder, ... }:
+{ config, mediaLibraryDir, nasRootFolder, ... }:
 
 {
   homelab.managedDirectories.entries = {
@@ -20,5 +20,32 @@
       group = "lab";
       mode = "0755";
     };
+  };
+
+  virtualisation.oci-containers.containers.jellyfin = {
+    image = "jellyfin/jellyfin:2026041305@sha256:7381f54b16aa544e02d33193ae43fbe0d1bc7470e179d576ee5d8d874e4952ca";
+
+    autoStart = true;
+
+    ports = [ "8096:8096/tcp" ];
+
+    volumes = [
+      "/srv/jellyfin/config:/config"
+      "/srv/jellyfin/cache:/cache"
+      "${mediaLibraryDir}:/media-library:ro"
+    ];
+
+    environment = {
+      JELLYFIN_PublishedServerUrl = "https://jellyfin.media.allanshomelab.com";
+    };
+
+    user = "${toString config.users.users.lab.uid}:${toString config.users.groups.lab.gid}";
+
+    extraOptions = [ "--replace" ];
+  };
+
+  systemd.services.podman-jellyfin = {
+    requires = [ "homelab-task-managed-state-restore.service" ];
+    after = [ "homelab-task-managed-state-restore.service" ];
   };
 }
