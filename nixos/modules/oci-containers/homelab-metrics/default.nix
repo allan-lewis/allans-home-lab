7df7977cad9233f9dbaf599b5dc5a2a648b80f3f
Allan Lewis <allan.e.lewis@gmail.com>
2026-05-26 12:20:01 +0000

    upgrade metrics version

--- nixos/modules/oci-containers/homelab-metrics/default.nix
+++ nixos/modules/oci-containers/homelab-metrics/default.nix
@@ -1,6 +1,18 @@
 {
   services.homelab.containers.homelab-metrics = {
+    image = "docker.io/allanelewis/homelab-metrics:v2026.05.1@sha256:ade6c0e969afde26b0c63b37f23844f4c23d213a90545311288eeb2f61f41c40";
+
     port = 9102;
-    image = "docker.io/allanelewis/homelab-metrics:v2026.05.0@sha256:9b015429aafedbbc80493c3477b5ddec95ac92d128c9c15733a859ad79cee030";
+
+    environment = {
+      ENABLE_NIXOS_METRICS = "true";
+    };
+
+    volumes = [
+      "/nix/store:/nix/store:ro"
+      "/nix/var/nix/profiles:/nix/var/nix/profiles:ro"
+      "/run/current-system:/run/current-system:ro"
+      "/run/booted-system:/run/booted-system:ro"
+    ];
   };
 }
