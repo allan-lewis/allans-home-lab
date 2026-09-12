17407feda730b75771dd44fbe6171783a61c06ca
Allan Lewis <allan.e.lewis@gmail.com>
2026-09-12 00:44:24 +0000

    bellatrix

---
+++ nixos/hosts/bellatrix/default.nix
@@ -0,0 +1,34 @@
+{ hostIp4Address, hostName, hostInterface, nixosVersion, lib, remoteBackupRoot, ... }:
+
+{
+  imports = [
+    ../../modules/bare-metal
+    ../../modules/tailscale
+
+    ../../profiles/desktop
+    ../../profiles/devops
+  ];
+
+  _module.args = {
+    dopplerConfig = "prd";
+    dopplerProject = "homelab";
+    dopplerTokenKey = "homelab_prd";
+  };
+
+  networking.hostName = hostName;
+  system.stateVersion = nixosVersion;
+
+  homelab.bareMetal = {
+    interface = hostInterface;
+    address = hostIp4Address;
+  };
+
+  time.timeZone = lib.mkForce "America/New_York";
+
+  services.homelab.managedState.schedule = "*:05";
+
+  homelab.sshKeyForLabUser = true;
+
+  homelab.labUser.enablePassword = true;
+}
+
